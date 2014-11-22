dc2fe754c2c85f42590ec2e4a9b1d632dcd60e85
Jack Jennings <j@ckjennin.gs>
2014-11-22 09:47:17 -0800

    basic functionality

--- lib/rack/a_day_without.rb
+++ lib/rack/a_day_without.rb
@@ -1,16 +1,35 @@
+require "date"
 require "rack"
 require "rack/a_day_without/version"
 
 module Rack
   class ADayWithout
 
-    def initialize app, options = {}
+    def initialize app, subject, options = {}
       @app = app
-      @options = options
+      @subject = subject
+      @date = Date.parse(options[:on].to_s)
+      @content = parse_content(options)
     end
 
     def call env
-      @app.call env
+      if @date == Date.today
+        res = Response.new
+        res.write @content
+        res.finish
+      else
+        @app.call env
+      end
+    end
+
+    private
+
+    def parse_content options
+      if options[:file]
+        ::File.read(options[:file])
+      else
+        options[:content] || ''
+      end
     end
 
   end
--- test/test_rack_a_day_without.rb
+++ test/test_rack_a_day_without.rb
@@ -13,11 +13,57 @@ describe Rack::ADayWithout do
       end
     end
 
-    it 'acts as a middleware' do
-      endpoint = Rack::ADayWithout.new @app
+    it 'blocks a request' do
+      endpoint = Rack::ADayWithout.new @app, 'Art', on: Date.today
+      status, headers, body = endpoint.call(Rack::MockRequest.env_for('/bar'))
+      status.must_equal 200
+      body.body.must_equal ['']
+    end
+
+    it 'passes a request' do
+      endpoint = Rack::ADayWithout.new @app, 'Art', on: Date.new
       status, headers, body = endpoint.call(Rack::MockRequest.env_for('/bar'))
       status.must_equal 200
       body.body.must_equal ['Downstream app']
+    end
+
+    it 'passes supplied content' do
+      endpoint = Rack::ADayWithout.new @app, 'Art',
+                                       on: Date.today,
+                                       content: 'foobar'
+      status, headers, body = endpoint.call(Rack::MockRequest.env_for('/bar'))
+      status.must_equal 200
+      body.body.must_equal ['foobar']
+    end
+
+    it 'passes supplied file' do
+      path = 'test/fixtures/index.html'
+      content = File.read(path)
+      endpoint = Rack::ADayWithout.new @app, 'Art',
+                                       on: Date.today,
+                                       file: path
+      status, headers, body = endpoint.call(Rack::MockRequest.env_for('/bar'))
+      status.must_equal 200
+      body.body.must_equal [content]
+    end
+  end
+
+  describe 'when initialized' do
+    it 'parses a date' do
+      date = '20/10/2014'
+      endpoint = Rack::ADayWithout.new @app, 'Art', on: date
+      endpoint.instance_variable_get('@date').must_equal Date.parse(date)
+    end
+
+    it 'parses a date' do
+      date = '20/10/2014'
+      endpoint = Rack::ADayWithout.new @app, 'Art', on: date
+      endpoint.instance_variable_get('@date').must_equal Date.parse(date)
+    end
+
+    it 'stores a subject' do
+      endpoint = Rack::ADayWithout.new @app, 'Art', on: Date.new
+      endpoint.instance_variable_get('@subject').must_equal 'Art'
     end
   end
 
